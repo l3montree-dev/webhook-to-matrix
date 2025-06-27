9e99e5fb5f66565173473beb175c88f92be66925
Sebastian Kawelke <sebastian.kawelke@l3montree.com>
2025-06-27 16:51:08 +0200

    Adds basic option for webhook secret
    Signed-off-by: Sebastian Kawelke <sebastian.kawelke@l3montree.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Webhook to Matrix Bot
+
+![DevGuard Badge](https://api.main.devguard.org/api/v1/badges/cvss/05bcc0c3-98fc-4d7a-a438-0d1e404f62c3)
+
+This little Golang bot listens to a webhook and sends the received data to a Matrix room.
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/l3montree-dev/webhook-to-matrix/pkg/api"
@@ -11,13 +12,31 @@ import (
 
 const Port = 5001
 
+func healthCheckHandler(w http.ResponseWriter, _ *http.Request) {
+	// answer a json response with a 200 OK status
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status":"ok"}`))
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	http.HandleFunc("/webhook/glitchtip", api.GlitchTipHandler)
+	// Check if required environment variables are set
+	requiredVars := []string{"ROOM_ID", "ACCESS_TOKEN", "HOME_SERVER", "WEBHOOK_SECRET"}
+	for _, v := range requiredVars {
+		if value := os.Getenv(v); value == "" {
+			log.Fatalf("Environment variable %s is not set", v)
+		}
+	}
+	log.Println("All required environment variables are set.")
+
+	http.HandleFunc("/health", healthCheckHandler)
+	// set the webhook secret to all paths
+	http.HandleFunc(fmt.Sprintf("/webhook/%s/glitchtip", os.Getenv("WEBHOOK_SECRET")), api.GlitchTipHandler)
 	addr := fmt.Sprintf("0.0.0.0:%d", Port)
 	log.Printf("Listening at %s", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
